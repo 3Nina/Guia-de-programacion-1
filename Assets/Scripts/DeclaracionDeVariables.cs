6136680dbe1b22c04aa6b43590f567a12eca8278
3Nina <ninamuhafara@gmail.com>
2025-03-17 09:07:48 -0300

    Ejercicio 1 - Nina

---
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+    int cantAulas = 3;
+    float tamanio = 12.4;
+    string nombreAula = "Rosa";
+    bool meTocaAhi = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
